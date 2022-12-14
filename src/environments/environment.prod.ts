3b3db06a8719ef80ae723a75290f8ebae49ee18a
mudasirqazi-dev <mudasirqazi.dev@gmail.com>
2022-12-14 13:50:57 +0500

    added production api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: 'http://localhost:3000/api/',
-  imagePath: 'http://localhost:3000/api',
-  login_url: 'http://localhost:3000/api/',
+  api_url: 'https://app-reviewers-api.vercel.app/api/',
+  imagePath: 'https://app-reviewers-api.vercel.app/api/',
+  login_url: 'https://app-reviewers-api.vercel.app/api/',
 };
